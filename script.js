d930b331dcac146cac8ea6597fcd624d2d5229d2
lemangomeister <maguinto1221@gmail.com>
2023-04-23 18:50:20 -0700

    blank

--- script.js
+++ script.js
@@ -58,5 +58,5 @@ document.getElementById("ask").addEventListener("click", function() {
     document.getElementById("answer").textContent = randomAnswer;
 
     document.getElementById("ball").classList.remove("shake");
-  }, 6000);
+  }, 6000); 
 });

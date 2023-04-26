e5b23da71960653d3010b1fb425ad8cad8551032
dolivares11 <dolivares@ucsd.edu>
2023-04-26 11:59:40 -0700

    Sample Questions Feature

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function sample1()
+{
+    var question = document.getElementById('question');
+    question.value = "Will I have a good day?";
+}
+
+function sample2()
+{
+    var question = document.getElementById('question');
+    question.value = "Will I pass my exam?";
+}
+
+function sample3()
+{
+    var question = document.getElementById('question');
+    question.value = "Should I call off work?";
+}
+
